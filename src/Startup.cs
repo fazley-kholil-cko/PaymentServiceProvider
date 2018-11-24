a1a56e89bcd8ee966a42ab82e8f58ce1a4b998f5
fazley-kholil-cko <fazley.kholil@checkout.com>
2018-11-24 13:42:14 +0400

    add indexing

--- src/Startup.cs
+++ src/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using PaymentServiceProvider.Indexing;
 
 namespace PaymentServiceProvider
 {
@@ -24,6 +25,9 @@ namespace PaymentServiceProvider
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            //add indexing
+            services.AddSingleton(new EsClient());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
